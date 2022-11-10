2bfdafa27e242814b3c416f0c777c042e3fdee1d
Marc Lomibao <MarcJerome.lomibao@gmail.com>
2022-11-10 13:01:38 +1300

    Update MessageAudioManager.cs

--- ahhhh!_this_water_is_scary!/Assets/Marc's Level Design Assets/Scripts/MessageAudioManager.cs
+++ ahhhh!_this_water_is_scary!/Assets/Marc's Level Design Assets/Scripts/MessageAudioManager.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class MessageAudioManager : MonoBehaviour
 {
+    private AudioSource audioSource;
+    [SerializeField]
+    AudioClip firstMessage;
+    bool firstMessagePlayed;
+
+    AudioClip secondMessage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource.GetComponent<AudioSource>();
+        audioSource.playOnAwake = true;
+        firstMessagePlayed = false;
+                StartCoroutine(playStartingMessages());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void PlaySoundOnce(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+
+    IEnumerator playStartingMessages()
+    {
+        if (!firstMessagePlayed)
+        {
+            audioSource.PlayOneShot(firstMessage);
+            yield return new WaitForSeconds(firstMessage.length);
+            firstMessagePlayed = true;
+            audioSource.PlayOneShot(secondMessage);
+        }
     }
 }
